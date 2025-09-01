2f1533b4c212746e5305abb5c9b62afefafc5f42
Tomer Aberbach <tomer@aberba.ch>
2025-09-01 19:13:24 -0400

    feat: overhaul and require node 22 (#2)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,25 +1,10 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkDirective from 'remark-directive'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
-import { DEFAULT_ADMONITION_TYPES, remarkAdmonition } from '../src/index.js'
+import { expect, test } from 'vitest'
+import { DEFAULT_ADMONITION_TYPES, remarkAdmonition } from './index.js'
 
 test(`remarkAdmonition works without options`, () => {
   const html = String(
